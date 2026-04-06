47a9ec5e51a10a4c567aec2505df72e83f0f27c0
recon3 <jch200325@naver.com>
2026-04-06 16:21:46 +0900

    BOJ 10844 Clear

---
+++ DP/10844.py
@@ -0,0 +1,14 @@
+n=int(input())
+
+dp=[[0]*10 for j in range(n)]
+
+for i in range(1,10):
+    dp[0][i]=1
+
+for i in range(1,n):
+    for j in range(10):
+        if j-1>=0:
+            dp[i][j]+=dp[i-1][j-1]
+        if j+1<=9:
+            dp[i][j]+=dp[i-1][j+1]
+print(sum(dp[-1])%1000000000)
