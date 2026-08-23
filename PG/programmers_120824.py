259b31d373d668d5eb8ec28954f7f873218011b1
inha030902 <cih327856@naver.com>
2026-08-23 21:33:46 +0900

    짝수 홀수 개수

--- PG/programmers_120824.py
+++ PG/programmers_120824.py
@@ -1,4 +1,4 @@
 def solution(num_list):
     even_count = sum(1 for x in num_list if x % 2 == 0)
     odd_count = sum(1 for x in num_list if x % 2 != 0)
-    return [even_count, odd_count]
+    return [even_count, odd_count
